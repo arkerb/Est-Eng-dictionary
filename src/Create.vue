0217bd8e39c89dce8a34a8a1052701584c0a84ab
Artur Kerb <arkerb@taltech.ee>
2020-06-29 18:34:54 +0300

    Added abilty to save words to localstorage

--- src/Create.vue
+++ src/Create.vue
@@ -3,21 +3,86 @@
         <form>
             <div class="form-group">
                 <label for="word">Word</label>
-                <input type="text" class="form-control" id="word" placeholder="Type a word">
+                <input v-model='word' type="text" class="form-control" id="word" placeholder="Type a word" required="required">
             </div>
             <div class="form-group">
                 <label for="explanation">Translation</label>
-                <input type="text" class="form-control" id="explanation" placeholder="Translate the word">
+                <input v-model='translation' type="text" class="form-control" id="explanation" placeholder="Translate the word" required="required">
             </div>
             <div class="form-group">
                 <label for="language">What language is the word written in?</label>
-                <select class="form-control" id="language">
+                <select v-model='language' class="form-control" id="language" required="required">
                     <option>English</option>
                     <option>Estonian</option>
                 </select>
             </div>
-            <button type="submit" class="btn btn-secondary mb-2 ">Submit word</button>
         </form>
+        <button v-on:click='createword' class="btn btn-secondary mb-2 ">Submit word</button>
+        Estonian {{estonian}}
+        <br>
+        English {{english}}
     </div>
 </template>
 
+<script>
+    export default {
+        name: 'app',
+        el: '#app',
+        data: function() {
+            return {
+                estonian: {},
+                english: {},
+                message: 'sdasd',
+                word: '',
+                translation: '',
+                language: '',
+                loaded: false,
+                values: [],
+            }
+        },
+        mounted() {
+            if (localStorage.estonian || localStorage.english) {
+                this.estonian = JSON.parse(localStorage.estonian);
+                this.english = JSON.parse(localStorage.english);
+            }
+        },
+        methods: {
+            createword: function () {
+                this.values = [];
+                if (this.word && this.language && this.translation) {
+                    if (this.language === "Estonian"){
+                        if (!(this.word in this.estonian)) {
+                            this.estonian[this.word] = this.translation;
+                            this.english[this.translation] = this.word;
+                        }else if (!this.estonian[this.word].includes(this.translation)){
+                            this.values = this.estonian[this.word] + ", " + this.translation;
+                            this.estonian[this.word] =this.values;
+                            if (!(this.translation in this.english)){
+                                this.english[this.translation] = this.word;
+                            }else{
+                                this.values = this.english[this.translation] + ", " + this.word;
+                                this.english[this.translation] = this.values;
+                            }
+                        }
+                    }else{
+                        if (!(this.word in this.english)) {
+                            this.english[this.word] = this.translation;
+                            this.estonian[this.translation] = this.word;
+                        }else if (!this.english[this.word].includes(this.translation)){
+                            this.values = this.english[this.word] + ", " + this.translation;
+                            this.english[this.word] =this.values;
+                            if (!(this.translation in this.estonian)){
+                                this.estonian[this.translation] = this.word;
+                            }else{
+                                this.values = this.estonian[this.translation] + ", " + this.word;
+                                this.estonian[this.translation] = this.values;
+                            }
+                        }
+                    }
+                }
+                localStorage.estonian = JSON.stringify(this.estonian);
+                localStorage.english = JSON.stringify(this.english);
+            }
+        }
+    }
+</script>
--- src/Find.vue
+++ src/Find.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-        <form>
+        <form
+                id="app"
+                name="app"
+        >
             <div class="row">
                 <div class="col">
                     <label for="word">Word</label>
-                    <input type="text" class="form-control" id="word" placeholder="Type a word to translate">
+                    <input type="text" v-model='word' class="form-control" id="word" placeholder="Type a word to translate">
                 </div>
                 <div class="col">
                     <div class="form-group">
                         <label for="language">What language is the word written in?</label>
-                        <select class="form-control" id="language">
+                        <select v-model="language" class="form-control" id="language">
                             <option>English</option>
                             <option>Estonian</option>
                         </select>
@@ -18,17 +21,45 @@
                 <div class="col">
                     <div class="form-group">
                         <label for="filter">Filter search</label>
-                        <select class="form-control" id="filter">
+                        <select v-model='filter' class="form-control" id="filter">
                             <option>Exact match</option>
                             <option>Must contain</option>
                         </select>
                     </div>
                 </div>
             </div>
-            <div class>
-                <button type="submit" class="btn btn-secondary mb-2 align-items-center">Find Translation</button>
-            </div>
         </form>
+        <button v-on:click='findword' class="btn btn-secondary mb-2 align-items-center">Find Translation</button>
+        <p v-if="loaded">{{estonian}}</p>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'app',
+        el: '#app',
+        data: function() {
+            return {
+                estonian: {},
+                english: {},
+                results: '',
+                word: '',
+                language: '',
+                filter: '',
+                loaded: false,
+            }
+        },
+        mounted() {
+            if (localStorage.estonian || localStorage.english) {
+                this.estonian = JSON.parse(localStorage.estonian);
+                this.english = JSON.parse(localStorage.english);
+            }
+        },
+        methods: {
+            findword: function () {
+                this.loaded = true;
+            }
+        }
+    }
+</script>
 
